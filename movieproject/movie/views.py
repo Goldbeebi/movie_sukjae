4b503571c607685e8ce666d668115b4d0abc3bb6
Goldbeebi <goldbeebij@gmail.com>
2022-07-07 22:24:14 +0900

    환경세팅 완료

--- movieproject/movie/views.py
+++ movieproject/movie/views.py
@@ -1,4 +1,43 @@
+from urllib import response
 from django.shortcuts import render
 
+import requests
+import json
+from .forms import SearchForm
+
+my_id = '5483d0bc39332d679bebaab12539c408'
+
 def home(request):
-    return render(request, 'index.html')
+    if request.method =='POST':
+        #입력된 내용으로
+        #https://api.themoviedb.org/3/search/movie?api_key=5483d0bc39332d679bebaab12539c408&language=en-US&page=1&include_adult=false
+        #이 형태의 url로 get 요청을 보내게 됩니다.
+
+        form = SearchForm(request.POST)
+        searchword = request.POST.get('search')
+    
+        if form.is_valid():
+            url = "https://api.themoviedb.org/3/search/movie?api_key=" + my_id + '&query=' + searchword
+            response = requests.get(url)
+            resdata = response.text
+
+            obj = json.loads(resdata)
+            obj = obj['results']
+
+            return render(request, 'search.html', {'obj': obj})
+    
+    else:
+        form = SearchForm()
+        url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=' + my_id
+        response = requests.get(url)
+        resdata = response.text
+        obj = json.loads(resdata)
+        obj = obj['results']
+    return render(request, 'index.html', {'obj' : obj, 'form':form})
+
+
+def detail(request,movie_id):
+    url = 'https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + my_id
+    response = requests.get(url)
+    resdata = response.text
+    return render(request, 'detail.html',{'resdata' : resdata})
